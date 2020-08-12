d129cc868d5ab03ce1c0ac0c43ff43993aa0eb5e
Muhd Hisham <muhdhisham@pop-os.localdomain>
2020-08-12 21:36:02 +0530

    created landing page and menu page

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("ydfguzj");
+function enterSite() {
+    $("#landing").css("transform", "translate(-200vw)");
+    $("#dimmed-bg").css("visibility","visible");
+    $("#dimmed-bg").css("transform", "translate(0)");
+    $("#above-fold").css("opacity","1");
+    $("#full-site").css("visibility","visible");
+    $("#full-site").css("overflow","auto");
+    $("#full-site").css("max-height","auto");
+
+}
