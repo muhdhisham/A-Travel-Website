c9c3ad482d96bbd01e32a51fa77b847a9b0d994b
Muhd Hisham <muhdhisham@pop-os.localdomain>
2020-08-13 22:38:48 +0530

    navigation,registration and first section

--- script.js
+++ script.js
@@ -4,7 +4,38 @@ function enterSite() {
     $("#dimmed-bg").css("transform", "translate(0)");
     $("#above-fold").css("opacity","1");
     $("#full-site").css("visibility","visible");
-    $("#full-site").css("overflow","auto");
+    $("#full-site").css("overflow","visible");
     $("#full-site").css("max-height","auto");
+    $("#register-side-bar").css("transform","translate(0)");
+}
 
+
+$("#visible-section").mouseenter(function(){openForm();blurr();});
+$("#empty-section").mouseenter(function(){closeForm();blurrCancel();});
+$("#form-section").mouseleave(function(){closeForm();blurrCancel();});
+
+
+function openForm(){
+    $("#register-side-bar").css("transform","translate(-400px)");
+    $("#register-side-bar").css("transition-delay","0s");
+    $("#register-side-bar").css("z-index","100");
+    $("#call-num").css("opacity","0");
+    $("#reg").css("opacity","0");
+}
+
+function closeForm(){
+    $("#register-side-bar").css("transform","translate(0)");
+    $("#register-side-bar").css("z-index","0");
+    $("#call-num").css("opacity","1");
+    $("#reg").css("opacity","1");
+}
+
+function blurr(){
+    $("#above-fold").css("opacity","0.5");
+    $("#above-fold").css("text-shadow","0 0 5px rgba(255,255,255, 0.2)");
+    // $("#above-fold").css("trabsition",("all 0s 0s"))
+}
+function blurrCancel(){
+    $("#above-fold").css("opacity","1");
+    $("#above-fold").css("text-shadow","0 0 0px rgba(0,0,0, 1)");
 }
