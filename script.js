e595e6a71518b563251384311e6a8e7d1936004b
Muhd Hisham <muhdhisham@pop-os.localdomain>
2020-08-15 21:02:00 +0530

    completed till search section

--- script.js
+++ script.js
@@ -32,10 +32,41 @@ function closeForm(){
 
 function blurr(){
     $("#above-fold").css("opacity","0.5");
-    $("#above-fold").css("text-shadow","0 0 5px rgba(255,255,255, 0.2)");
-    // $("#above-fold").css("trabsition",("all 0s 0s"))
+    $("#above-fold").css("text-shadow","0 0 15px rgba(255,255,500, 0.03)");
+    $("#above-fold").css("transition",("all 0.3s 0s"))
 }
 function blurrCancel(){
     $("#above-fold").css("opacity","1");
     $("#above-fold").css("text-shadow","0 0 0px rgba(0,0,0, 1)");
 }
+
+
+// a self calling  function
+
+(function manageTotalUsers() {
+    
+    let totalUsers = String(getRandom(100000, 200000));
+    totalUsers = addComma(totalUsers);
+    
+    addToTotalUsers(totalUsers);
+    
+    function addToTotalUsers(currentValue) {
+        let num = Number(currentValue.replace(",", ""));
+        num++;
+        num = String(num);
+        num = addComma(num);
+        $("#total-users").html(num);
+        setTimeout(function() {
+            addToTotalUsers(num);
+        }, getRandom(100, 5000));
+    }
+    
+    function getRandom(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    
+    function addComma(num) {
+        return num.slice(0, 3) + "," + num.slice(3, 6);
+    }
+    
+})();
